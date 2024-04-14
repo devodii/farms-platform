d7c088a1208ee4cd40f7c9b804ebb10024952494
devodii <emmanuelodii80@gmail.com>
2024-04-14 05:53:14 +0100

    chore: add sub domain helper

--- src/app/[domain]/page.tsx
+++ src/app/[domain]/page.tsx
@@ -1,9 +1,19 @@
 // maps to subdomain.
 
-export default function HostedFarmPage() {
+import { getSubDomain } from "@/lib/domain";
+
+interface Props {
+  params: {
+    domain: string;
+  };
+}
+
+export default async function HostedFarmPage({ params }: Props) {
+  const farm = getSubDomain(params?.domain);
+
   return (
     <section className="min-h-screen flex items-center justify-center">
-      This is a hosted farm page.
+      This is a hosted farm page for {farm}
     </section>
   );
 }
---
+++ src/lib/domain.ts
@@ -0,0 +1,1 @@
+export const getSubDomain = (host: string) => host?.split(".")?.[0];
